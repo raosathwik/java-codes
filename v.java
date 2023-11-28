ef1ae3f4adf47ccfb0f981f65224fdfad73d6b8d
raosathwik3@gmail.com <raosathwik3@gmail.com>
2023-11-28 22:24:14 +0530

    push

---
+++ v.java
@@ -0,0 +1,14 @@
+public class v{
+    public static void main(String[] args) {
+        for (int i = 1; i >= 1; i--) {
+        for (int k = 7; k >= i; k--) {
+        System.out.print(" "); 
+        }
+        System.out.print(i); 
+        for (int j = 1; j <= i*2; j++) {
+        System.out.print(" "); 
+        }
+        System.out.println(i); 
+        }
+        }
+}
