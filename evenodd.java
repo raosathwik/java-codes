b9986cd8e0cb050dd908542d02d40460ec4319e7
raosathwik3@gmail.com <raosathwik3@gmail.com>
2023-11-29 22:59:41 +0530

    push

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+public class evenodd {
+    public static void main(String[] args) {
+        int n=11;
+        if(n%2 == 0){
+            System.out.print("Even");
+        }
+        else{
+            System.out.print("odd");
+        }
+    }
+}
