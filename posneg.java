83ff84ffdd15b4aa101fe8f040ad267baf0b8b9d
raosathwik3@gmail.com <raosathwik3@gmail.com>
2023-11-30 19:37:56 +0530

    push

---
+++ posneg.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class posneg {
+    public static void main(String[] args) {
+        int n=-0;
+       
+        if(n>0){
+            System.out.println("Positive");
+        }
+        else if(n<0){
+            System.out.println("Negetive");
+        }
+        else{
+            System.out.println("zero");
+        }
+    }
+}
