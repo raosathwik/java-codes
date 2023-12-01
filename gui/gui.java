64480f5747817304985438cd49a84f3b23680d45
raosathwik3@gmail.com <raosathwik3@gmail.com>
2023-12-01 19:03:24 +0530

    push

---
+++ gui/gui.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class gui{
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello"+ name);
+
+
+
+      int age  =  Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+       JOptionPane.showMessageDialog(null, "you are "+age+ "years old");
+    }
+}
