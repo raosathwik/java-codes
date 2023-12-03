f039d796957b2af1768df00b131ca7f65044ff33
raosathwik3@gmail.com <raosathwik3@gmail.com>
2023-12-03 23:37:23 +0530

    push

---
+++ factrecursion.java
@@ -0,0 +1,14 @@
+public class factrecursion {
+    public static void main(String[] args) {
+        
+        System.out.println(factorial(5));
+    }
+
+    public static int factorial(int number){
+        if(number==0){
+            return 1;
+        }
+        return number * factorial(number-1);
+    }
+    }
+
