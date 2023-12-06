e82cac5f6568e1824a6749205eca4074d325e02f
raosathwik3@gmail.com <raosathwik3@gmail.com>
2023-12-06 18:46:54 +0530

    push

---
+++ factlarge.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+public class factlarge {
+    public static void main(String[] args) {
+        int number=20;
+        BigInteger fact = BigInteger.ONE;
+        for(int i=1;i<=number;i++){
+            fact=fact.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(fact);
+    }
+}
