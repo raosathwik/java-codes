269e5a08672ea04b4edaa48e9aa40c567ad86ca1
raosathwik3@gmail.com <raosathwik3@gmail.com>
2024-01-04 22:17:34 +0530

    push

---
+++ fibonacci1.java
@@ -0,0 +1,15 @@
+public class fibonacci1 {
+    public static void main(String[] args) {
+        int count = 10;
+        int [] fib = new int[count];
+        fib[0]=0;
+        fib[1]=1;
+
+        for(int i=2;i<count;i++){
+            fib[i] = fib[i-1]+fib[i-2];
+        }
+        for(int i : fib){
+            System.out.println(i);
+        }
+    }
+}
